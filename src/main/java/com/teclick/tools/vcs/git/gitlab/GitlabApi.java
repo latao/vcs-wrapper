95d3e7d424f05ad54c7d0ed0a7b7fa5fd88f8b3d
Nelson <lipeng36@sina.com>
2017-06-06 00:05:24 +0800

    Reconstruction

--- src/main/java/com/teclick/tools/vcs/git/gitlab/GitlabApi.java
+++ src/main/java/com/teclick/tools/vcs/git/gitlab/GitlabApi.java
@@ -1,5 +1,6 @@
 package com.teclick.tools.vcs.git.gitlab;
 
+import com.teclick.tools.vcs.git.GitException;
 import com.teclick.tools.vcs.git.gitlab.entity.*;
 
 import javax.ws.rs.*;
@@ -9,23 +10,23 @@ import java.util.List;
 
 /**
  * Created by Nelson on 2017-05-31 14:03.
- * GitlabApi
+ * GitLabApi
  */
 @Consumes({MediaType.APPLICATION_JSON})
 @Produces({MediaType.APPLICATION_JSON + ";charset=UTF-8"})
-public interface GitlabApi {
+public interface GitLabApi {
 
     @POST
     @Path("session")
-    Session login(@QueryParam("login") String account, @QueryParam("password") String password) throws GitlabException;
+    Session login(@QueryParam("login") String account, @QueryParam("password") String password) throws GitException;
 
     @GET
     @Path("projects")
-    List<Project> getProjects() throws GitlabException;
+    List<Project> getProjects() throws GitException;
 
     @GET
     @Path("projects/{projectId}/repository/branches")
-    List<Branch> getBranches(@PathParam("projectId") int projectId) throws GitlabException;
+    List<Branch> getBranches(@PathParam("projectId") int projectId) throws GitException;
 
     @GET
     @Path("projects/{projectId}/repository/commits")
@@ -33,23 +34,23 @@ public interface GitlabApi {
                             @QueryParam("ref_name") String ref_name,
                             @QueryParam("path") String path,
                             @QueryParam("since") String since,
-                            @QueryParam("until") String until) throws GitlabException;
+                            @QueryParam("until") String until) throws GitException;
 
     @GET
     @Path("projects/{projectId}/repository/commits/{sha}")
-    Commit getCommits(@PathParam("projectId") int projectId, @PathParam("sha") String sha) throws GitlabException;
+    Commit getCommits(@PathParam("projectId") int projectId, @PathParam("sha") String sha) throws GitException;
 
     @GET
     @Path("projects/{projectId}/repository/archive.zip")
-    Response getRepositoryArchive(@PathParam("projectId") int projectId, @QueryParam("sha") String sha) throws GitlabException;
+    Response getRepositoryArchive(@PathParam("projectId") int projectId, @QueryParam("sha") String sha) throws GitException;
 
     @GET
     @Path("namespaces")
-    List<Namespace> searchNamespace(@QueryParam("search") String groupName) throws GitlabException;
+    List<Namespace> searchNamespace(@QueryParam("search") String groupName) throws GitException;
 
     @POST
     @Path("projects")
     Project createProject(@QueryParam("name") String projectName, @QueryParam("namespace_id") int namespaceId,
-                          @QueryParam("description") String description, @QueryParam("public") boolean isPublic) throws GitlabException;
+                          @QueryParam("description") String description, @QueryParam("public") boolean isPublic) throws GitException;
 
 }
--- src/main/java/com/teclick/tools/vcs/git/gitlab/GitlabApiClient.java
+++ src/main/java/com/teclick/tools/vcs/git/gitlab/GitlabApiClient.java
@@ -1,136 +1,37 @@
 package com.teclick.tools.vcs.git.gitlab;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import com.teclick.tools.vcs.git.gitlab.entity.*;
-import org.apache.cxf.interceptor.LoggingInInterceptor;
-import org.apache.cxf.interceptor.LoggingOutInterceptor;
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import com.teclick.tools.vcs.git.GitClientBase;
+import com.teclick.tools.vcs.git.GitException;
+import com.teclick.tools.vcs.git.gitlab.entity.Session;
 import org.apache.cxf.jaxrs.client.WebClient;
-import org.apache.cxf.transport.http.HTTPConduit;
-import org.apache.cxf.transports.http.configuration.HTTPClientPolicy;
-
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by Nelson on 2017-05-31 14:04.
- * GitlabApiClient
+ * GitLabApiClient
  */
-public class GitlabApiClient implements GitlabApi {
+public class GitLabApiClient extends GitClientBase {
 
-    private final GitlabApi gitlabApi;
-
-    public GitlabApiClient(String url, String account, String password) throws GitlabException {
-
-        JacksonJsonProvider provider = new JacksonJsonProvider();
-        provider.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        gitlabApi = JAXRSClientFactory.create(url, GitlabApi.class, Collections.singletonList(provider), true);
-
-        if (System.getProperty("debug", "").equals("true")) {
-            WebClient.getConfig(gitlabApi).getInInterceptors().add(new LoggingInInterceptor());
-            WebClient.getConfig(gitlabApi).getOutInterceptors().add(new LoggingOutInterceptor());
-        }
-
-        login(account, password);
+    public GitLabApi getGitLabApi() {
+        return gitLabApi;
     }
 
-    public GitlabApiClient(String url, String account, String password, int timeoutInSecond) throws GitlabException {
-        this(url, account, password);
+    private GitLabApi gitLabApi;
 
-        HTTPConduit conduit = WebClient.getConfig(gitlabApi).getHttpConduit();
-        HTTPClientPolicy policy = new HTTPClientPolicy();
-        policy.setReceiveTimeout(timeoutInSecond * 1000);
-        conduit.setClient(policy);
+    @SuppressWarnings("unchecked")
+    public GitLabApiClient(String url, String account, String password) throws GitException {
+        super(url, account, password, GitLabApi.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public GitLabApiClient(String url, String account, String password, int timeoutInSecond) throws GitException {
+        super(url, account, password, timeoutInSecond, GitLabApi.class);
     }
 
     @Override
-    public Session login(String account, String password) throws GitlabException {
-        try {
-            Session session = gitlabApi.login(account, password);
-            WebClient.client(gitlabApi).header("PRIVATE-TOKEN", session.getPrivate_token());
-            return session;
-        } catch (WebApplicationException e) {
-            throw new GitlabException("login", e);
-        }
-    }
-
-    @Override
-    public List<Project> getProjects() throws GitlabException {
-        try {
-            return gitlabApi.getProjects();
-        } catch (WebApplicationException e) {
-            throw new GitlabException("getProjects", e);
-        }
-    }
-
-    @Override
-    public List<Branch> getBranches(int projectId) throws GitlabException {
-        try {
-            return gitlabApi.getBranches(projectId);
-        } catch (WebApplicationException e) {
-            throw new GitlabException("getBranches", e);
-        }
-    }
-
-    @Override
-    public List<Commit> getCommits(int projectId, String ref_name, String path, String since, String until) throws GitlabException {
-        try {
-            return gitlabApi.getCommits(projectId, ref_name, path, since, until);
-        } catch (WebApplicationException e) {
-            throw new GitlabException("getCommits", e);
-        }
-    }
-
-    @Override
-    public Commit getCommits(int projectId, String sha) throws GitlabException {
-        try {
-            return gitlabApi.getCommits(projectId, sha);
-        } catch (WebApplicationException e) {
-            throw new GitlabException("getCommits", e);
-        }
-    }
-
-    @Override
-    public Response getRepositoryArchive(int projectId, String sha) throws GitlabException {
-        try {
-            return gitlabApi.getRepositoryArchive(projectId, sha);
-        } catch (WebApplicationException e) {
-            throw new GitlabException("getRepositoryArchive", e);
-        }
-    }
-
-    @Override
-    public List<Namespace> searchNamespace(String groupName) throws GitlabException {
-        try {
-            return gitlabApi.searchNamespace(groupName);
-        } catch (WebApplicationException e) {
-            throw new GitlabException("searchNamespace", e);
-        }
-    }
-
-    @Override
-    public Project createProject(String projectName, int namespaceId, String description, boolean isPublic) throws GitlabException {
-        try {
-            return gitlabApi.createProject(projectName, namespaceId, description, isPublic);
-        } catch (WebApplicationException e) {
-            throw new GitlabException("searchNamespace", e);
-        }
-    }
-
-    private String getFileName(MultivaluedMap<String, String> header) {
-        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
-        for (String filename : contentDisposition) {
-            if ((filename.trim().startsWith("filename"))) {
-                String[] name = filename.split("=");
-                return name[1].trim().replaceAll("\"", "");
-            }
-        }
-        return "unknown";
+    protected void addAuthorizationHeader(String account, String password) throws GitException {
+        gitLabApi = (GitLabApi) client;
+        Session session = gitLabApi.login(account, password);
+        WebClient.client(gitLabApi).header("PRIVATE-TOKEN", session.getPrivate_token());
     }
 
 }
